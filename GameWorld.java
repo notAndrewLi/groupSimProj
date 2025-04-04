84bfa0e28e3311e91df7f646b7996743b56eb0c3
Andrew <341177673@gapps.yrdsb.ca>
2025-04-04 08:43:21 -0400

    Added background image

--- GameWorld.java
+++ GameWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class GameWorld extends World
 {
-
+    GreenfootImage bg = new GreenfootImage("images/background.jpg");
     /**
      * Constructor for objects of class GameWorld.
      * 
@@ -16,6 +16,7 @@ public class GameWorld extends World
     public GameWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1024, 768, 1, true);
+        setBackground(bg);
     }
 }
