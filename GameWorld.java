a02e7d7085ca3819ac3b8d8e53e28d7b0d035206
GITcyrus <mrcyrusyu@gmail.com>
2025-04-16 08:38:07 -0400

    Re-implemented isOpponent Boolean
    - Fighters have an "isOpponent" boolean
    - The difference being the world dictates whether a fighter is "an opponent" or not

--- GameWorld.java
+++ GameWorld.java
@@ -34,6 +34,8 @@ public class GameWorld extends World
         MC = spawnHero(customizationType, upgradeBonuses);
         
         OPP = spawnOpponent();
+        
+        OPP.setAsOpponent();
     }
     
     public void act(){
